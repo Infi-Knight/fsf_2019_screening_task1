c81570810dc944447c8ec7416b11261a66038ffd
Ravi Soni <soniravi829@gmail.com>
2019-03-31 17:46:23 +0530

    Init team model

--- todos/models.py
+++ todos/models.py
@@ -1,6 +1,8 @@
+import datetime
+
 from django.db import models
 from django.contrib.auth import get_user_model
-
+from django.urls import reverse
 # DRY
 UserModel = get_user_model()
 
@@ -32,6 +34,34 @@ class Task(models.Model):
     def __str__(self):
         return self.title
 
+    def get_absolute_url(self):
+        return reverse("task_manager:task_description", kwargs={"task_id": self.id})
+
+    def has_duedate_passed(self):
+        """Returns whether the Tasks's due date has passed or not."""
+        if self.due_on and datetime.date.today() > self.due_on:
+            return True
+        else:
+            return False
+
     class Meta:
         ordering = ["planned_on"]
         verbose_name_plural = "Tasks"
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=50)
+    desc = models.TextField(default='', blank=True)
+    leader = models.ForeignKey(
+        UserModel, on_delete=models.CASCADE, related_name='leads_teams')
+    members = models.ManyToManyField(UserModel)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("task_manager:team_description", kwargs={"team_id": self.id})
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name_plural = "Teams"
